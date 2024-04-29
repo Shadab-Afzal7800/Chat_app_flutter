7e68bb2851e946d05139ee859944bc5028885022
Shadab-Afzal7800 <shadabafzal7800@gmail.com>
2024-04-29 19:55:50 +0530

    made some corrections

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
-import 'package:chat_app_flutter/pages/homepage.dart';
+import 'package:chat_app_flutter/pages/complete_profile.dart';
+import 'package:chat_app_flutter/pages/create_account.dart';
+import 'package:chat_app_flutter/pages/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: CompleteProfileScreen(),
     );
   }
 }
